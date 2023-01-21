03c45ac110e961f0775b47ff9f987993a1eefa44
Melia Masek <Masekmelia@gmail.com>
2023-01-20 23:51:48 -0700

    Updated Inventory Script

--- 3DGame/Assets/Scripts/3DGame/InventorySlot.cs
+++ 3DGame/Assets/Scripts/3DGame/InventorySlot.cs
@@ -1,8 +1,50 @@
 using UnityEngine;
-public class InventorySlot : ScriptableObject
+public class InventorySlot : MonoBehaviour
 {
-    public InventoryData ItemData;
-    public int stacksize;
-    
+    public InventoryData itemData;
+    public InventoryData ItemData => itemData;
 
+    public int stacksize;
+    public int StackSize => stacksize;
+
+    public InventorySlot(InventoryData source, int amount)
+    {
+        itemData = source;
+        stacksize = amount;
+    }
+
+    public InventorySlot()
+    {
+        ClearSlot();
+    }
+
+    public void ClearSlot()
+    {
+        itemData = null;
+        stacksize = -1;
+    }
+
+    public bool RoomLeftInStack(int amountToAdd, out int amountRemaining)
+    {
+        amountRemaining = ItemData.maxStackSize - stacksize;
+        return RoomLeftInStack(amountToAdd);
+    }
+
+    public bool RoomLeftInStack(int amountToAdd)
+    {
+        if (stacksize + amountToAdd <= itemData.maxStackSize)
+            return true;
+        else return false;
+    }
+    
+    public void AddToStack(int amount)
+    {
+        stacksize += amount;
+    }
+
+    public void RemoveFromStack(int amount)
+    {
+        stacksize -= amount;
+    }
 }
+
