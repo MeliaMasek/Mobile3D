295c0237e7c7a80cbc567bffdfd6ff2866d7bb96
Melia Masek <Masekmelia@gmail.com>
2023-02-06 12:28:35 -0700

    Added Floor instantiation with random floor tiles

--- 3DGame/Assets/Scripts/RepeatBackground.cs
+++ 3DGame/Assets/Scripts/RepeatBackground.cs
@@ -1,30 +1,80 @@
+using System.Collections.Generic;
 using UnityEngine;
-
+// code borrowed and modified from N3K EN on youtube https://www.youtube.com/watch?v=HIsEqKPoJXM
+// code borrowed and modified from N3K EN on youtube https://www.youtube.com/watch?v=WnvW6m4Fqmo&list=PLLH3mUGkfFCXps_IYvtPcE9vcvqmGMpRK&index=8
 public class RepeatBackground : MonoBehaviour
 {
-    private BoxCollider bCollider;
-    private Rigidbody rb;
-    private float width;
-    private float scrollSpeed = -1f;
+    public GameObject[] floorPrefab;
+    private Transform playerTransform;
+    private List<GameObject> activeTiles;
+    
+    public float floorLength = 10f;
+    public int floorAmount = 2;
+    public float safeZone = 10f;
 
-    private void Start()
+    private float spawnZ = -5f;
+    private int lastPrefabIndex = 0;
+   
+    public void Update()
     {
-        bCollider = GetComponent<BoxCollider>();
-        rb = GetComponent<Rigidbody>();
-
-        width = bCollider.size.z;
-        bCollider.enabled = false;
-
-        rb.velocity = new Vector3(0, 0, scrollSpeed);
+        if (playerTransform.position.z - safeZone > (spawnZ - floorAmount * floorLength))
+        {
+            SpawnFloor();
+            DeleteFloor();
+        }
     }
 
-    private void Update()
+    public void Start()
     {
-        if (transform.position.z < -width)
+        activeTiles = new List<GameObject>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        for (int i = 0; i < floorAmount; i++)
         {
-            Vector3 resetPostion = new Vector3(0, 0, width * 10f);
-            transform.position = (Vector3)transform.position + resetPostion;
-
+            if (i < 2)
+            {
+                SpawnFloor(0);
+            }
+            else
+            {
+                SpawnFloor();
+            }
         }
+    }
+
+    public void SpawnFloor(int prefabIndex = -1)
+    {
+        GameObject go;
+        if (prefabIndex == -1)
+        {
+            go = Instantiate(floorPrefab[RandomPrefabIndex()]) as GameObject;
+        }
+        else
+        {
+            go = Instantiate(floorPrefab[prefabIndex] as GameObject);
+        }
+        go.transform.SetParent(transform);
+        go.transform.position =Vector3.forward * spawnZ;
+        spawnZ += floorLength;
+        activeTiles.Add(go);
+    }
+
+    public void DeleteFloor()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+
+    private int RandomPrefabIndex()
+    {
+        if (floorPrefab.Length <= 1)
+            return 0;
+        int randonIndex = lastPrefabIndex;
+        while (randonIndex == lastPrefabIndex)
+        {
+            randonIndex = Random.Range(0, floorPrefab.Length);
+        }
+
+        lastPrefabIndex = randonIndex;
+        return randonIndex;
     }
 }
