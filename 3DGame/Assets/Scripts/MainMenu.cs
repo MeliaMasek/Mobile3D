a2d4f268717f1349a7a91580b6d380b307a880dc
Melia Masek <Masekmelia@gmail.com>
2023-02-27 14:00:55 -0700

    Added Home button function to pause menu

--- 3DGame/Assets/Scripts/MainMenu.cs
+++ 3DGame/Assets/Scripts/MainMenu.cs
@@ -2,28 +2,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 //code borrowed and modified by Hooson on youtube https://www.youtube.com/watch?v=tfzwyNS1LUY
-public class PauseMenu : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject pauseMenu;
     [SerializeField] private GameObject mainMenu;
-
     private int sceneToContinue;
     private int currentSceneIndex;
-    public void Pause()
-    {
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0f;
-    }
 
     public void Resume()
     {
-        pauseMenu.SetActive(false);
+        mainMenu.SetActive(false);
         Time.timeScale = 1f;
-    }
-
-    public void Home()
-    {
-        Time.timeScale = 1f;
-        mainMenu.SetActive(true);
     }
 }
