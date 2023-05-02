d33028afd677ef3170b0d7817df5191a58317e85
Melia Masek <Masekmelia@gmail.com>
2023-05-02 00:38:04 -0600

    Prefab updates

--- 3DGame/Assets/Scripts/Store/IStoreItem.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-public interface IStoreItem
-{
-    public int Price { get; set; }
-    public bool Purchased { get; set; }
-    public Sprite PreviewArt { get; set; }
-    
-    public string Name { get; set; }
-}
